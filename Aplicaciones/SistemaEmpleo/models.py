8ab4a8976b3a6a08454cb6d7fc80cf4802744dd2
DomenicaToaquiza <domenica.toaquiza9945@utc.edu.ec>
2025-07-05 17:23:45 -0500

    Model para el login

--- Aplicaciones/SistemaEmpleo/models.py
+++ Aplicaciones/SistemaEmpleo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Usuario(models.Model):
+    TIPO_USUARIO = [
+        ('buscador', 'Buscador de Empleo'),
+        ('empresa', 'Empresa'),
+    ]
+    usuario = models.CharField(max_length=50, unique=True)
+    clave = models.CharField(max_length=50)
+    tipo_usuario = models.CharField(max_length=10, choices=TIPO_USUARIO)
+
+    def __str__(self):
+        return f'{self.usuario} - {self.get_tipo_usuario_display()}'
