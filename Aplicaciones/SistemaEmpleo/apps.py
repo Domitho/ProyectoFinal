98462459182fe4d9f712649655fc01f2ac88a359
DomenicaToaquiza <domenica.toaquiza9945@utc.edu.ec>
2025-07-05 16:47:13 -0500

    Configuracion del proyecto

--- Aplicaciones/SistemaEmpleo/apps.py
+++ Aplicaciones/SistemaEmpleo/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SistemaempleoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'SistemaEmpleo'
+    name = 'Aplicaciones.SistemaEmpleo'
