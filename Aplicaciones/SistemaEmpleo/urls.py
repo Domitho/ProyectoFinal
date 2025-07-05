d330c4854121a0e365c9d6cefe91df7e81e48d9b
DomenicaToaquiza <domenica.toaquiza9945@utc.edu.ec>
2025-07-05 17:35:16 -0500

    Urls y views

--- Aplicaciones/SistemaEmpleo/urls.py
+++ Aplicaciones/SistemaEmpleo/urls.py
@@ -1,6 +1,15 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from Aplicaciones.SistemaEmpleo import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
 
+    # Login y registro
+    path('', views.login_view, name='login'),
+    path('registro/', views.registro_view, name='registro'),
+
+    # Pantallas de inicio
+    path('inicio-buscador/', views.inicio_buscador, name='inicio_buscador'),
+    path('inicio-empresa/', views.inicio_empresa, name='inicio_empresa'),
 ]
