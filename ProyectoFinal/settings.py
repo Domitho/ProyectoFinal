24984e8888cb8daf16254c4016374cd264871f8e
Domitho <domenica.toaquiza9945@utc.edu.ec>
2025-07-21 03:11:00 -0500

    Archivos

--- ProyectoFinal/settings.py
+++ ProyectoFinal/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-akivilb&#+prvjw)nyj%62vvv+tw1xu67nnmm9(y7&x_#u&l_m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
@@ -75,15 +75,11 @@ WSGI_APPLICATION = 'ProyectoFinal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'empleosdb',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',  
-        'PORT': '5432',  
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 # Password validation
