659b99d2a0ea0fecc6268e3500d2a19d806cfb0e
Domitho <domenica.toaquiza9945@utc.edu.ec>
2025-07-21 03:54:52 -0500

    Agrego STATIC_ROOT para producción en Render

--- ProyectoFinal/settings.py
+++ ProyectoFinal/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'ProyectoFinal/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'ProyectoFinal/media'
